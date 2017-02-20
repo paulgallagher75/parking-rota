1778f556acad8c635e800c83c688903f942d9d8a
Paul Gallagher <pgallagh@redhat.com>
2017-02-20 15:49:06 +0000

    Intial commit of allocate script

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# parking-rota
-Car parking rota system
+# Car parking rota systems
+Group of scripts utilizing google services to share a limited number of car parking spaces fairly among a group of people.
+
+## Allocation system
+The rota is driven by a CSV list of names and choice of days in order of preference example :
+
+```
+Dave,Mon,Tue,Fri
+Mark,Mon,Tue,Thur,Fri
+John,Mon,Tue,Wed,Fri
+Sarah,Mon,Tue,Wed,Fri
+```
+
+The output is displayed on the console and written to a google calandar of choice that can be shared with all the individuals.
+
+The rota is decided on a top down then bottom up. so running the allocator like this, with the list of name above with only 3 spaces to pick from,
+
+```sh
+./allocate.py -c choices.txt -d 2017-02-27 -s 3
+```
+
+results in this list below as the outcome, so MON has 4 people wanting it as first choice, as Dave is at the top of the list he gets Monday for Space 1 first, Mark is next on the list however as Space 1 has already gone for the Monday he gets his sencond choice which is Tuesday for Space 1, next is John who wanted Monday then Tuesday which have both already gone do he gets Thursday for Space 1, then Sarah gets Friday as all other choices for Space 1 are gone.
+
+The system then move onto Space 2 however starts at the bottom of the list, so Sarah gets Monday, and so on from bottom to top. After allocating Space 2 we move to the next Space 3 which is worked out from top to bottom and so on.
+
+After the intial pass over to fill up the spaces the system runs back over from the start 4 more time filling up any spaces it con with choices, which is why Mark has been allocated Thursday as well as Tuesday in Space 1
+
+```
+MON: Dave, Sarah, Mark
+TUE: Mark, John, Dave
+WED: John, Sarah, Free
+THU: Mark, Free, Free
+FRI: Sarah, Mark, John
+```
+
+The system is only fair if each week it is generated the list the rotated from bottom to top so Sarah would move to the top of the list next time we allocated.
+
+### Options
+|--config-file|CSV file with ordered list of names and days selected|
+|--add-calendar-entries|Automatically add the entries to the specified calendar|
+|--google-calendar|Name of the Google Calendar to update|
+|--date-of-monday|Date (YYYY-MM-DD) that the Monday starts on for this week|
+|--number-of-spaces|Number os car park spaces there are|
+
+## Email notification system
+
+TODO
+
+## Future plans
+- Upgrade to use web based choice so users can update the day preferences.
+- Automation of list rotation i.e. moving the person from the botton to top.
+- Automate email notification of new allocation.
---
+++ allocator/allocate.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+
+import csv
+import sys
+import os
+import time
+import datetime
+
+from oauth2client import client
+from googleapiclient import sample_tools
+from optparse import OptionParser
+
+service = None
+parking_selection = []
+spaces = []
+parking = {}
+parking_numbers = ["C6","C7","C8","C17","N/A"]
+
+cal_entry_tmpl = {
+    "summary": "",
+    "start": {
+        "date": "",
+    },
+    "end": {
+        "date": "",
+    }
+}
+
+def get_command_line_option_parser():
+    parser = OptionParser(usage='%prog [options]',
+                          description='Print out car park spaces and update Google calendar directly.')
+    parser.add_option('-c', '--config-file', dest='config_file',
+                      help='CSV file with ordered list of names and days selected.')
+    parser.add_option('--add-calendar-entries', dest='update_cal', default=False, action='store_true',
+                      help='Automatically add the entries to the specified calendar.')
+    parser.add_option('-g', '--google-calendar', dest='calendar', default='RedHat Car Parking',
+                      help='Name of the Google Calendar to update.')
+    parser.add_option('-d', '--date-of-monday', dest='sdate', default=time.strftime("%Y-%m-%d"),
+                      help='Date (YYYY-MM-DD) that the Monday starts on for this week.')
+    parser.add_option('-s', '--number-of-spaces', dest='num_spaces', default=3,
+                      help='Number os car park spaces there are (Default 3)')
+    return parser
+
+def init_spaces(num_spaces):
+    for i in range(int(num_spaces)):
+        spaces.append("Space " + str(i+1))
+        parking[spaces[i]] = ['Free','Free','Free','Free','Free']
+
+def day_to_number(day_string):
+    return {
+        'mon': 0,
+        'tue': 1,
+        'wed': 2,
+        'thur': 3,
+        'fri': 4}.get(day_string.lower(), 99)
+
+def pick_weeks_spaces(space, from_bottom=False):
+    tmp = parking_selection
+    if from_bottom:
+        tmp = reversed(parking_selection)
+    for entry in tmp:
+        name = entry[0]
+        for day_string in entry[1:]:
+            day_number = day_to_number(day_string)
+            if day_number != 99 and parking[space][day_number] == "Free":
+                already_has_another_space = False
+                #Check that they don't already have a spot in the other spaces
+                for i in spaces:
+                    if parking[i][day_number] == name:
+                        already_has_another_space = True
+                if not already_has_another_space:
+                    parking[space][day_number] = name
+                    break
+
+def get_google_cal_service():
+    global service
+    if not service:
+        # Authenticate and construct service.
+        service, flags = sample_tools.init(
+            '', 'calendar', 'v3', __doc__, __file__,
+            scope='https://www.googleapis.com/auth/calendar')
+    return service
+
+def get_cal_id_by_name(cal_name):
+    service = get_google_cal_service()
+    page_token = None
+    while True:
+        calendar_list = service.calendarList().list(pageToken=page_token).execute()
+        for calendar_list_entry in calendar_list['items']:
+            if calendar_list_entry['summary'] == cal_name:
+                return calendar_list_entry['id']
+        page_token = calendar_list.get('nextPageToken')
+        if not page_token:
+            break
+    return None
+
+def update_google_calendar(cal_id, date):
+    dates = ["","","","","",""]
+    last_date = date
+    #Need the Sat as the very last so the end date for the friday is the Sat
+    for i in range(6):
+        dates[i] = last_date
+        last_date = (datetime.datetime.strptime(last_date, "%Y-%m-%d") +
+                     datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    space_iter = 0
+    for key in sorted(parking):
+        count = 0
+        if space_iter >= len(parking_numbers):
+            pno = parking_numbers[-1]
+        else:
+            pno = parking_numbers[space_iter]
+        space_iter = space_iter + 1
+        for name in parking[key]:
+            cal_entry_tmpl["summary"] = name + " - " + pno
+            cal_entry_tmpl["start"]["date"] = dates[count]
+            cal_entry_tmpl["end"]["date"] = dates[count + 1]
+            count = count + 1
+            print cal_entry_tmpl
+
+            #try:
+            #    service.events().insert(calendarId=cal_id,
+            #                            body=cal_entry_tmpl).execute()
+            #except Exception, err:
+            #    print err
+
+def get_day_selection(csv_file):
+    #Get parking selections array
+    reader = csv.reader(open(csv_file))
+    for row in reader:
+        row[0] = row[0].replace('_', ' ')
+        if row[0].startswith('#'):
+            continue
+        if row[0] in parking_selection:
+            print row[0] + " in parking selection file multiple times"
+            return None
+        parking_selection.append(row)
+    return parking_selection
+
+def print_parking(num_spaces):
+    spaces_string = ["MON: ","TUE: ","WED: ","THU: ","FRI: " ]
+    for n in range(len(spaces_string)):
+        for i in range(int(num_spaces)):
+            if i == 0:
+                comma = ''
+            else:
+                comma = ', '
+            spaces_string[n] = spaces_string[n] + comma + parking[spaces[i]][n]
+        print spaces_string[n]
+
+def main(argv):
+    parser = get_command_line_option_parser()
+    (opts, extra) = parser.parse_args()
+
+    if not opts.config_file:
+        print 'Config file not specified.'
+        parser.print_help()
+        return 1
+
+    if not os.path.isfile(opts.config_file):
+        print "Config file " + os.path.abspath(opts.config_file) + " not found."
+        parser.print_help()
+        return 1
+
+    cal_id = get_cal_id_by_name(opts.calendar)
+    if not cal_id:
+        print "Calendar " + os.path.abspath(opts.config_file) + " not found."
+        parser.print_help()
+        return 1
+
+    parking_selection = get_day_selection(opts.config_file)
+    if not parking_selection:
+        parser.print_help()
+        return 1
+
+    init_spaces(opts.num_spaces)
+
+    #Build list up now top to bottom and then bottom to top
+    #Try a number of times to fill the spaces up with more than one pass
+    reverse = False
+    for i in range(int(opts.num_spaces)):
+        for n in range(1,5):
+            pick_weeks_spaces(spaces[i], reverse)
+        reverse = not reverse
+
+    print_parking(opts.num_spaces)
+
+    if opts.update_cal:
+        date = datetime.datetime.strptime(opts.sdate, "%Y-%m-%d")
+        if date.isoweekday() != 1:
+            print "Start of week date " + opts.sdate + " not Monday."
+            parser.print_help()
+            return 1
+        update_google_calendar(cal_id, opts.sdate)
+
+    return 0
+
+if __name__ == '__main__':
+    main(sys.argv)
